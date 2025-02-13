cdad86af4aa74cb545d0108390963eb96b803f94
Shiv Bhonde <shivbhonde04@gmail.com>
2025-02-13 16:27:43 +0530

    remove extra types

--- packages/nextjs/utils/eip712/register.ts
+++ packages/nextjs/utils/eip712/register.ts
@@ -1,23 +1,19 @@
 import { EIP_712_DOMAIN } from "./common";
 import { recoverTypedDataAddress } from "viem";
 
-export const EIP_712_TYPES__USER_REGISTER = {
-  Message: [
-    { name: "action", type: "string" },
-    { name: "description", type: "string" },
-  ],
-};
-
-export const EIP_712_MESSAGE__USER_REGISTER = {
-  action: "Register",
-  description: "I would like to register as a builder in speedrunethereum.com signing this offchain message",
-} as const;
-
 export const EIP_712_TYPED_DATA__USER_REGISTER = {
   domain: EIP_712_DOMAIN,
-  types: EIP_712_TYPES__USER_REGISTER,
+  types: {
+    Message: [
+      { name: "action", type: "string" },
+      { name: "description", type: "string" },
+    ],
+  },
   primaryType: "Message",
-  message: EIP_712_MESSAGE__USER_REGISTER,
+  message: {
+    action: "Register",
+    description: "I would like to register as a builder in speedrunethereum.com signing this offchain message",
+  },
 } as const;
 
 export const isValidEIP712UserRegisterSignature = async ({
