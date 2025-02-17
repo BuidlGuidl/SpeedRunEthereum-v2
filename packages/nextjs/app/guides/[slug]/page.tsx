b17b5716f60d59de229b0ceaaf63ca5a7583b971
Carlos Sánchez <oceanrdn@gmail.com>
2025-02-17 18:23:46 +0100

    Guides

---
+++ packages/nextjs/app/guides/[slug]/page.tsx
@@ -0,0 +1,28 @@
+import { notFound } from "next/navigation";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import { getAllGuidesSlugs, getGuideBySlug } from "~~/services/guides";
+
+export async function generateStaticParams() {
+  const slugs = await getAllGuidesSlugs();
+
+  return slugs;
+}
+
+export default function GuidePage({ params }: { params: { slug: string } }) {
+  const guide = getGuideBySlug(params.slug);
+
+  if (!guide) {
+    return notFound();
+  }
+
+  return (
+    <div className="flex flex-col gap-8 p-4 max-w-[900px] mx-auto">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-bold">{guide.title}</h1>
+      </div>
+      <div className="prose dark:prose-invert max-w-none">
+        <MDXRemote source={guide.content} />
+      </div>
+    </div>
+  );
+}
---
+++ packages/nextjs/services/guides.ts
@@ -0,0 +1,37 @@
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+
+const guidesDirectory = path.join(process.cwd(), "guides");
+
+export type GuideMetadata = {
+  title: string;
+  description: string;
+  slug: string;
+};
+
+export type Guide = GuideMetadata & {
+  content: string;
+};
+
+export function getAllGuidesSlugs(): string[] {
+  return fs.readdirSync(guidesDirectory).map(fileName => fileName.replace(/\.md$/, ""));
+}
+
+export function getGuideBySlug(slug: string): Guide | null {
+  const fullPath = path.join(guidesDirectory, `${slug}.md`);
+
+  try {
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const { data, content } = matter(fileContents);
+
+    return {
+      ...(data as GuideMetadata),
+      content,
+      slug,
+    };
+  } catch (error) {
+    // Not found.
+    return null;
+  }
+}
