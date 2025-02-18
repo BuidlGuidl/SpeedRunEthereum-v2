cad69a5dfae8f966873d8b85d0fd6bd899eeaff6
Shiv Bhonde <shivbhonde04@gmail.com>
2025-02-18 15:21:25 +0530

    configure basic schema

--- packages/nextjs/services/database/config/schema.ts
+++ packages/nextjs/services/database/config/schema.ts
@@ -1,8 +1,44 @@
-import { sql } from "drizzle-orm";
-import { pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+import { relations, sql } from "drizzle-orm";
+import { pgEnum, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const reviewActionEnum = pgEnum("review_action_enum", ["REJECTED", "ACCEPTED", "SUBMITTED"]);
+export const eventTypeEnum = pgEnum("event_type_enum", ["challenge.submit", "challenge.autograde"]);
 
 // TODO: Define the right schema.
 export const users = pgTable("users", {
   id: varchar("id", { length: 42 }).primaryKey(),
   creationTimestamp: timestamp("creation_timestamp").default(sql`now()`),
+});
+
+export const userChallenges = pgTable("user_challenges", {
+  userChallengeId: serial("userChallengeId").primaryKey(),
+  userAddress: varchar("userAddress", { length: 42 })
+    .notNull()
+    .references(() => users.id),
+  challengeCode: varchar("challengeCode", { length: 255 }).notNull(),
+  frontendUrl: varchar("frontendUrl", { length: 255 }),
+  contractUrl: varchar("contractUrl", { length: 255 }),
+  reviewComment: text("reviewComment"), // Feedback provided during from the autograder
+  submittedTimestamp: timestamp("submittedTimestamp").defaultNow(),
+  reviewAction: reviewActionEnum("reviewAction"), // Final review decision from autograder (REJECTED or ACCEPTED)
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  userChallenges: many(userChallenges),
+  events: many(events),
+}));
+
+export const userChallengesRelations = relations(userChallenges, ({ one }) => ({
+  user: one(users, {
+    fields: [userChallenges.userAddress],
+    references: [users.id],
+  }),
+}));
+
+export const events = pgTable("events", {
+  eventId: serial("eventId").primaryKey(),
+  eventType: eventTypeEnum("eventType").notNull(),
+  eventTimestamp: timestamp("eventTimestamp").defaultNow(),
+  userAddress: varchar("userAddress", { length: 42 }).notNull(),
+  challengeCode: varchar("challengeCode", { length: 255 }),
 });
