ed3d8e77e1a35676b9218b1a3748ed5618744828
Shiv Bhonde <shivbhonde04@gmail.com>
2025-02-19 17:37:10 +0530

    separte out hook logic in useSubmitChallenge hook

--- packages/nextjs/app/challenge/[challengeId]/_components/SubmitChallengeModal.tsx
+++ packages/nextjs/app/challenge/[challengeId]/_components/SubmitChallengeModal.tsx
@@ -1,14 +1,7 @@
 import { forwardRef, useState } from "react";
-import { useRouter } from "next/navigation";
-import { useMutation } from "@tanstack/react-query";
-import { useAccount, useSignTypedData } from "wagmi";
 import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 import { InputBase } from "~~/components/scaffold-eth";
-import { submitChallenge } from "~~/services/api/challenges";
-import { EIP_712_TYPED_DATA__CHALLENGE_SUBMIT } from "~~/services/eip712/challenge";
-import { notification } from "~~/utils/scaffold-eth";
-
-const VALID_BLOCK_EXPLORER_HOSTS = ["sepolia.etherscan.io", "sepolia-optimism.etherscan.io"];
+import { useSubmitChallenge } from "~~/hooks/useSubmitChallenge";
 
 type SubmitChallengeModalProps = {
   challengeId: string;
@@ -18,55 +11,10 @@ type SubmitChallengeModalProps = {
 export const SubmitChallengeModal = forwardRef<HTMLDialogElement, SubmitChallengeModalProps>(
   ({ closeModal, challengeId }, ref) => {
     const [frontendUrl, setFrontendUrl] = useState("");
-    const [etherscanUrl, setEtherscanUrl] = useState("");
-    const router = useRouter();
+    const [contractUrl, setContractUrl] = useState("");
 
-    const { address } = useAccount();
-    const { signTypedDataAsync } = useSignTypedData();
-
-    const { mutate: submit, isPending } = useMutation({
-      mutationFn: async () => {
-        if (!address) throw new Error("Wallet not connected");
-        try {
-          new URL(frontendUrl);
-          const etherscan = new URL(etherscanUrl);
-
-          if (!VALID_BLOCK_EXPLORER_HOSTS.includes(etherscan.host)) {
-            throw new Error("Please use a valid Etherscan testnet URL (Sepolia)");
-          }
-        } catch (e) {
-          throw new Error("Please enter valid URLs");
-        }
-
-        const message = {
-          ...EIP_712_TYPED_DATA__CHALLENGE_SUBMIT.message,
-          challengeId,
-          frontendUrl,
-          contractUrl: etherscanUrl,
-        };
-
-        const signature = await signTypedDataAsync({
-          ...EIP_712_TYPED_DATA__CHALLENGE_SUBMIT,
-          message,
-        });
-
-        return submitChallenge({
-          challengeId,
-          userAddress: address,
-          frontendUrl,
-          contractUrl: etherscanUrl,
-          signature,
-        });
-      },
-      onSuccess: () => {
-        notification.success("Challenge submitted successfully!");
-        router.push(`/builders/${address}`);
-        router.refresh();
-        closeModal();
-      },
-      onError: (error: Error) => {
-        notification.error(error.message);
-      },
+    const { submitChallenge, isPending } = useSubmitChallenge({
+      onSuccess: closeModal,
     });
 
     return (
@@ -107,18 +55,16 @@ export const SubmitChallengeModal = forwardRef<HTMLDialogElement, SubmitChalleng
               </div>
               <InputBase
                 placeholder="https://sepolia.etherscan.io/address/**YourContractAddress**"
-                value={etherscanUrl}
-                onChange={e => {
-                  setEtherscanUrl(e);
-                }}
+                value={contractUrl}
+                onChange={e => setContractUrl(e)}
               />
             </div>
 
             <div className="modal-action">
               <button
                 className="btn btn-primary self-center"
-                disabled={!frontendUrl || !etherscanUrl || isPending}
-                onClick={() => submit()}
+                disabled={!Boolean(frontendUrl && contractUrl) || isPending}
+                onClick={() => submitChallenge({ challengeId, frontendUrl, contractUrl })}
               >
                 {isPending ? (
                   <>
