77cf65f9478ce32e88780d2ccc5df784cf309e3a
Carlos Sánchez <oceanrdn@gmail.com>
2025-02-21 18:54:43 +0100

    punk blockie

--- packages/nextjs/app/builders/[address]/page.tsx
+++ packages/nextjs/app/builders/[address]/page.tsx
@@ -1,9 +1,15 @@
 import { UserChallengesTable } from "../_component/UserChallengesTable";
+import PunkBlockie from "~~/components/PunkBlockie";
 import { findUserChallengesByAddress } from "~~/services/database/repositories/userChallenges";
 
 export default async function BuilderPage({ params }: { params: { address: string } }) {
   const { address: userAddress } = params;
   const challenges = await findUserChallengesByAddress(userAddress);
 
-  return <UserChallengesTable challenges={challenges} />;
+  return (
+    <div>
+      <PunkBlockie address={userAddress} scale={2} />
+      <UserChallengesTable challenges={challenges} />
+    </div>
+  );
 }
