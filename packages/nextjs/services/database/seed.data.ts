7cc689acf9f83c3800709665d6962688b6a5e8a3
Rinat <rinat@hey.com>
2025-02-27 13:17:46 +0300

    fix: remove base image url

--- packages/nextjs/services/database/seed.data.ts
+++ packages/nextjs/services/database/seed.data.ts
@@ -43,8 +43,6 @@ export const seedUsers: (typeof users.$inferInsert)[] = [
   },
 ];
 
-const BASE_IMAGE_URL = "https://github.com/BuidlGuidl/SpeedRunEthereum-v2/packages/nextjs/public";
-
 export const seedChallenges: (typeof challenges.$inferInsert)[] = [
   {
     id: ChallengeId.SIMPLE_NFT_EXAMPLE,
@@ -54,7 +52,7 @@ export const seedChallenges: (typeof challenges.$inferInsert)[] = [
     description:
       "🎫 Create a simple NFT to learn basics of 🏗 scaffold-eth. You'll use 👷‍♀️ HardHat to compile and deploy smart contracts. Then, you'll use a template React app full of important Ethereum components and hooks. Finally, you'll deploy an NFT to a public network to share with friends! 🚀",
     sortOrder: 0,
-    previewImage: `${BASE_IMAGE_URL}/assets/challenges/simpleNFT.svg`,
+    previewImage: "/assets/challenges/simpleNFT.svg",
     dependencies: [],
   },
   {
@@ -65,7 +63,7 @@ export const seedChallenges: (typeof challenges.$inferInsert)[] = [
     description:
       "🦸 A superpower of Ethereum is allowing you, the builder, to create a simple set of rules that an adversarial group of players can use to work together. In this challenge, you create a decentralized application where users can coordinate a group funding effort. The users only have to trust the code.",
     sortOrder: 1,
-    previewImage: `${BASE_IMAGE_URL}/assets/challenges/stakingToken.svg`,
+    previewImage: "/assets/challenges/stakingToken.svg",
     dependencies: [],
   },
   {
@@ -76,9 +74,9 @@ export const seedChallenges: (typeof challenges.$inferInsert)[] = [
     description:
       '🤖 Smart contracts are kind of like "always on" vending machines that anyone can access. Let\'s make a decentralized, digital currency (an ERC20 token). Then, let\'s build an unstoppable vending machine that will buy and sell the currency. We\'ll learn about the "approve" pattern for ERC20s and how contract to contract interactions work.',
     sortOrder: 2,
-    previewImage: `${BASE_IMAGE_URL}/assets/challenges/tokenVendor.svg`,
+    previewImage: "/assets/challenges/tokenVendor.svg",
     dependencies: [],
-    icon: `${BASE_IMAGE_URL}/assets/key_icon.svg`,
+    icon: "/assets/key_icon.svg",
   },
   {
     id: ChallengeId.DICE_GAME,
@@ -88,7 +86,7 @@ export const seedChallenges: (typeof challenges.$inferInsert)[] = [
     description:
       "🎰 Randomness is tricky on a public deterministic blockchain. The block hash is the result proof-of-work (for now) and some builders use this as a weak form of randomness.  In this challenge you will take advantage of a Dice Game contract by predicting the randomness in order to only roll winning dice!",
     sortOrder: 3,
-    previewImage: `${BASE_IMAGE_URL}/assets/challenges/diceGame.svg`,
+    previewImage: "/assets/challenges/diceGame.svg",
     dependencies: [ChallengeId.SIMPLE_NFT_EXAMPLE, ChallengeId.DECENTRALIZED_STAKING, ChallengeId.TOKEN_VENDOR],
   },
   {
@@ -99,7 +97,7 @@ export const seedChallenges: (typeof challenges.$inferInsert)[] = [
     description:
       "💵 Build an exchange that swaps ETH to tokens and tokens to ETH. 💰 This is possible because the smart contract holds reserves of both assets and has a price function based on the ratio of the reserves. Liquidity providers are issued a token that represents their share of the reserves and fees...",
     sortOrder: 4,
-    previewImage: `${BASE_IMAGE_URL}/assets/challenges/dex.svg`,
+    previewImage: "/assets/challenges/dex.svg",
     dependencies: [
       ChallengeId.SIMPLE_NFT_EXAMPLE,
       ChallengeId.DECENTRALIZED_STAKING,
@@ -115,7 +113,7 @@ export const seedChallenges: (typeof challenges.$inferInsert)[] = [
     description:
       "🛣️ The Ethereum blockchain has great decentralization & security properties but these properties come at a price: transaction throughput is low, and transactions can be expensive. This makes many traditional web applications infeasible on a blockchain... or does it?  State channels look to solve these problems by allowing participants to securely transact off-chain while keeping interaction with Ethereum Mainnet at a minimum.",
     sortOrder: 5,
-    previewImage: `${BASE_IMAGE_URL}/assets/challenges/state.svg`,
+    previewImage: "/assets/challenges/state.svg",
     dependencies: [
       ChallengeId.SIMPLE_NFT_EXAMPLE,
       ChallengeId.DECENTRALIZED_STAKING,
@@ -131,7 +129,7 @@ export const seedChallenges: (typeof challenges.$inferInsert)[] = [
     description:
       '👩‍👩‍👧‍👧 Using a smart contract as a wallet we can secure assets by requiring multiple accounts to "vote" on transactions. The contract will keep track of transactions in an array of structs and owners will confirm or reject each one. Any transaction with enough confirmations can "execute".',
     sortOrder: 6,
-    previewImage: `${BASE_IMAGE_URL}/assets/challenges/multiSig.svg`,
+    previewImage: "/assets/challenges/multiSig.svg",
     dependencies: [
       ChallengeId.SIMPLE_NFT_EXAMPLE,
       ChallengeId.DECENTRALIZED_STAKING,
@@ -151,7 +149,7 @@ export const seedChallenges: (typeof challenges.$inferInsert)[] = [
     description:
       "🎨 Create a dynamic SVG NFT using a smart contract. Your contract will generate on-chain SVG images and allow users to mint their unique NFTs. ✨ Customize your SVG graphics and metadata directly within the smart contract. 🚀 Share the minting URL once your project is live!",
     sortOrder: 7,
-    previewImage: `${BASE_IMAGE_URL}/assets/challenges/dynamicSvgNFT.svg`,
+    previewImage: "/assets/challenges/dynamicSvgNFT.svg",
     dependencies: [
       ChallengeId.SIMPLE_NFT_EXAMPLE,
       ChallengeId.DECENTRALIZED_STAKING,
