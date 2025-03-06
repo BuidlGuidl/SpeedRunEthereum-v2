56a0c89068a9168da786eb6bcce7046a15163f5c
Rinat <rinat@hey.com>
2025-03-06 18:24:23 +0300

    refactor: JoinBgButton props

--- packages/nextjs/app/_components/JoinBGButton.tsx
+++ packages/nextjs/app/_components/JoinBGButton.tsx
@@ -6,19 +6,32 @@ import PadLockIcon from "../_assets/icons/PadLockIcon";
 import QuestionIcon from "../_assets/icons/QuestionIcon";
 import { useAccount, useWalletClient } from "wagmi";
 import { useJoinBg } from "~~/hooks/useJoinBg";
-import { UserRole } from "~~/services/database/config/types";
+import { ChallengeId, UserRole } from "~~/services/database/config/types";
+import { UserChallenges } from "~~/services/database/repositories/userChallenges";
 import { UserByAddress } from "~~/services/database/repositories/users";
+import { getChallengeDependenciesInfo } from "~~/utils/dependent-challenges";
+
+const JOIN_BG_DEPENDENCIES = [
+  ChallengeId.SIMPLE_NFT_EXAMPLE,
+  ChallengeId.DECENTRALIZED_STAKING,
+  ChallengeId.TOKEN_VENDOR,
+  ChallengeId.DICE_GAME,
+  ChallengeId.MINIMUM_VIABLE_EXCHANGE,
+];
 
 type JoinBGProps = {
-  isLocked: boolean;
   user?: UserByAddress;
-  lockReasonToolTip: string;
+  userChallenges?: UserChallenges;
 };
 
-const JoinBGButton: React.FC<JoinBGProps> = ({ isLocked, user, lockReasonToolTip }) => {
+const JoinBGButton = ({ user, userChallenges = [] }: JoinBGProps) => {
   const { address: connectedAddress } = useAccount();
   const { data: walletClient } = useWalletClient();
   const { handleJoinBg, isJoiningBg } = useJoinBg({ user });
+  const { completed: dependenciesCompleted, lockReasonToolTip } = getChallengeDependenciesInfo({
+    dependencies: JOIN_BG_DEPENDENCIES,
+    userChallenges,
+  });
 
   const builderAlreadyJoined = Boolean(user?.role === UserRole.BUILDER);
 
@@ -45,16 +58,16 @@ const JoinBGButton: React.FC<JoinBGProps> = ({ isLocked, user, lockReasonToolTip
   return (
     <>
       <button
-        disabled={isLocked || builderAlreadyJoined}
+        disabled={!dependenciesCompleted || builderAlreadyJoined}
         className="flex justify-center text-[#088484] bg-[#C8F5FF] items-center text-xl lg:text-lg px-4 py-1 border-2 border-[#088484] rounded-full disabled:opacity-70"
         onClick={handleJoinBg}
       >
-        {isLocked && <PadLockIcon className="w-6 h-6 mr-2" />}
+        {!dependenciesCompleted && <PadLockIcon className="w-6 h-6 mr-2" />}
         {isJoiningBg && <span className="mr-2">Joining...</span>}
         {!isJoiningBg && !builderAlreadyJoined && <span>Join the 🏰️ BuidlGuidl</span>}
         {builderAlreadyJoined && <span>Already joined</span>}
       </button>
-      {isLocked && (
+      {!dependenciesCompleted && (
         <div className="tooltip relative cursor-pointer ml-2 text-[#088484]" data-tip={lockReasonToolTip}>
           <QuestionIcon className="h-8 w-8" />
         </div>
