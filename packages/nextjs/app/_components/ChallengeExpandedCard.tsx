5f00f686b7f9acc0ea805fbf56a001bc384d4171
Carlos Sánchez <oceanrdn@gmail.com>
2025-03-19 10:55:04 +0100

    RWD tweaks on homepage

--- packages/nextjs/app/_components/ChallengeExpandedCard.tsx
+++ packages/nextjs/app/_components/ChallengeExpandedCard.tsx
@@ -42,7 +42,7 @@ const ChallengeExpandedCard = ({
   return (
     <div className="challenge-expanded-card flex justify-center group relative">
       <div className="flex justify-between max-w-7xl py-8 mx-10 sm:mx-14 pl-10 border-primary border-l-[3px] sm:border-l-[5px] relative flex-col-reverse lg:flex-row border-b-2 group-[:not(:has(+.challenge-expanded-card))]:border-b-0">
-        <div className="hidden group-first:block absolute -left-3 z-10 top-0 w-[18px] h-[58%] lg:h-[50%] bg-base-200" />
+        <div className="hidden group-first:block absolute -left-3 z-10 top-0 w-[18px] h-[57%] sm:h-[50%] bg-base-200" />
         <div className="flex flex-col max-w-full lg:max-w-[40%] gap-18 lg:gap-20">
           <div className="flex flex-col items-start gap-0">
             <div className="h-6">
@@ -57,7 +57,7 @@ const ChallengeExpandedCard = ({
             <h2 className="text-xl lg:text-2xl font-medium mt-0">{challenge.challengeName}</h2>
           </div>
           <div className="flex flex-col gap-8">
-            <span className="text-base leading-[1.5]">{challenge.description}</span>
+            <span className="text-sm lg:text-base leading-[1.5]">{challenge.description}</span>
             <div className="flex items-center">
               {comingSoon && (
                 <button
@@ -150,10 +150,10 @@ const ChallengeExpandedCard = ({
             <p className="p-3 text-center">{challengeId} image</p>
           )}
         </div>
-        <span className="absolute h-5 w-5 rounded-full bg-base-300 border-primary border-4 top-[58%] lg:top-[50%] -left-[11px] sm:-left-[13px]" />
+        <span className="absolute h-3 w-3 sm:h-5 sm:w-5 rounded-full bg-base-300 border-primary border-2 lg:border-4 top-[57%] sm:top-[50%] -left-[8px] sm:-left-[13px]" />
         {challenge.icon && (
           <Image
-            className="absolute h-6 w-5 bg-no-repeat bg-[20px_auto] top-[58%] lg:top-[50%] -left-[38px] sm:-left-[40px]"
+            className="absolute w-4 h-4 sm:h-6 sm:w-5 bg-no-repeat bg-[20px_auto] top-[57%] sm:top-[50%] -left-[32px] sm:-left-[40px]"
             src={challenge.icon}
             alt={challenge.icon}
             width={24}
