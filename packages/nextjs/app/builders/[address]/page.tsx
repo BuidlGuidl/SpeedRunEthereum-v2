0bd7b2772deaaa866662f1497885d93d3a61aa8c
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2025-04-10 15:11:59 -0600

    add not found to builders route

--- packages/nextjs/app/builders/[address]/page.tsx
+++ packages/nextjs/app/builders/[address]/page.tsx
@@ -1,7 +1,7 @@
+import { notFound } from "next/navigation";
 import { UpgradedToBGCard } from "./_components/UpgradedToBGCard";
 import { UserChallengesTable } from "./_components/UserChallengesTable";
 import { UserProfileCard } from "./_components/UserProfileCard";
-import { NotFound } from "~~/components/NotFound";
 import { RouteRefresher } from "~~/components/RouteRefresher";
 import { isBgMember } from "~~/services/api-bg/builders";
 import { getLatestSubmissionPerChallengeByUser } from "~~/services/database/repositories/userChallenges";
@@ -14,17 +14,7 @@ export default async function BuilderPage({ params }: { params: { address: strin
   const bgMemberExists = await isBgMember(userAddress);
 
   if (!user) {
-    return (
-      <NotFound
-        heading="User Not Found"
-        extraElement={
-          <>
-            <p className="mb-0">Double check the Ethereum address is correct in the URL</p>
-            <p className="m-0">Make sure not to use an ENS address in the URL</p>
-          </>
-        }
-      />
-    );
+    notFound();
   }
 
   return (
