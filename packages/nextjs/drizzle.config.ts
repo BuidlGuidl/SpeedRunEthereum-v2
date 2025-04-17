8ca511f172ab4f8287a6fc0031c5f5818274ae77
Carlos Sánchez <oceanrdn@gmail.com>
2025-04-17 09:54:43 +0200

    y/N prompt

--- packages/nextjs/drizzle.config.ts
+++ packages/nextjs/drizzle.config.ts
@@ -1,13 +1,25 @@
 import * as dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
+import * as fs from "fs";
 
 dotenv.config({ path: ".env.development" });
 
 const PRODUCTION_DATABASE_HOSTNAME = "cold-resonance";
 
-if (process.env.POSTGRES_URL?.includes(PRODUCTION_DATABASE_HOSTNAME) && !process.argv.includes("--force")) {
-  console.log("\n⚠️ You are pointing to the production database. Use `--force` if you want to continue.\n");
-  process.exit(0);
+if (process.env.POSTGRES_URL?.includes(PRODUCTION_DATABASE_HOSTNAME)) {
+  process.stdout.write("\n⚠️ You are pointing to the production database. Are you sure you want to proceed? (y/N): ");
+
+  // Ensure blocking read
+  fs.readSync(0, Buffer.alloc(0), 0, 0, null);
+
+  const buffer = Buffer.alloc(1);
+  fs.readSync(0, buffer, 0, 1, null);
+  const answer = buffer.toString().toLowerCase();
+
+  if (answer !== "y") {
+    console.log("Aborted.");
+    process.exit(1);
+  }
 }
 
 export default defineConfig({
