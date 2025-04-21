aa1512191f850cd0f15abca4c721581d6035f3d2
Carlos Sánchez <oceanrdn@gmail.com>
2025-04-21 18:33:54 +0200

    detect isNextRuntime => If not, instanciate http client

--- packages/nextjs/services/database/config/postgresClient.ts
+++ packages/nextjs/services/database/config/postgresClient.ts
@@ -1,13 +1,19 @@
 import * as schema from "./schema";
-import { Pool as NeonPool } from "@neondatabase/serverless";
+import { Pool as NeonPool, neon } from "@neondatabase/serverless";
+import { drizzle as drizzleNeonHttp } from "drizzle-orm/neon-http";
 import { drizzle as drizzleNeon } from "drizzle-orm/neon-serverless";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
-type DbInstance = ReturnType<typeof drizzle<typeof schema>> | ReturnType<typeof drizzleNeon<typeof schema>>;
+type DbInstance =
+  | ReturnType<typeof drizzle<typeof schema>>
+  | ReturnType<typeof drizzleNeon<typeof schema>>
+  | ReturnType<typeof drizzleNeonHttp<typeof schema>>;
 
 let dbInstance: DbInstance | null = null;
 let poolInstance: Pool | NeonPool | null = null;
+
+const isNextRuntime = !!process.env.NEXT_RUNTIME;
 
 function getDb(): DbInstance {
   if (dbInstance) {
@@ -16,9 +22,17 @@ function getDb(): DbInstance {
 
   const NEON_DB_STRING = "neondb";
   if (process.env.POSTGRES_URL?.includes(NEON_DB_STRING)) {
-    poolInstance = new NeonPool({ connectionString: process.env.POSTGRES_URL as string });
-    dbInstance = drizzleNeon(poolInstance as NeonPool, { schema, casing: "snake_case" });
+    if (isNextRuntime) {
+      // Use neon-serverless for next runtimes so we can take advantage of the connection pooling on Neon
+      poolInstance = new NeonPool({ connectionString: process.env.POSTGRES_URL as string });
+      dbInstance = drizzleNeon(poolInstance as NeonPool, { schema, casing: "snake_case" });
+    } else {
+      // Use neon-http for non-next runtimes (e.g. scripts) so we can do batch operations
+      const sql = neon(process.env.POSTGRES_URL as string);
+      dbInstance = drizzleNeonHttp(sql, { schema, casing: "snake_case" });
+    }
   } else {
+    // Use node-postgres for non-neon environments (local)
     const pool = new Pool({
       connectionString: process.env.POSTGRES_URL,
     });
