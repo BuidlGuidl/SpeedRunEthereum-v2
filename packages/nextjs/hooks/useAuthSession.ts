77b6d96cbaf0479550e9725efa777e99aaa0d198
Shiv Bhonde <shivbhonde04@gmail.com>
2025-05-05 16:07:13 +0530

    use enum for comparison in useAuthSession file

--- packages/nextjs/hooks/useAuthSession.ts
+++ packages/nextjs/hooks/useAuthSession.ts
@@ -1,9 +1,10 @@
 import { UseSessionOptions, useSession } from "next-auth/react";
+import { UserRole } from "~~/services/database/config/types";
 
 export const useAuthSession = <R extends boolean>(options?: UseSessionOptions<R>) => {
   const sessionData = useSession(options);
 
-  const isAdmin = sessionData?.data?.user?.role === "admin";
+  const isAdmin = sessionData?.data?.user?.role === UserRole.ADMIN;
   const userAddress = sessionData?.data?.user?.userAddress;
   const isAuthenticated = sessionData.status === "authenticated";
 
