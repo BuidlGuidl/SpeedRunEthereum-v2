4ae7b6b113a14b0fb2d781324be4d274d674d89a
Shiv Bhonde <shivbhonde04@gmail.com>
2025-05-19 16:37:59 +0530

    add force dynamic for api/batches/name-list

--- packages/nextjs/app/api/batches/name-list/route.tsx
+++ packages/nextjs/app/api/batches/name-list/route.tsx
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { getBatchNameList } from "~~/services/database/repositories/batches";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const batches = await getBatchNameList();
