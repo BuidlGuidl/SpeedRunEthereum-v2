d4b279a2a5ddaf5bf2517b3ca98bda0e3989252d
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2025-06-06 12:48:40 +0800

    fix api url encoding

--- packages/nextjs/services/api/builds/index.ts
+++ packages/nextjs/services/api/builds/index.ts
@@ -10,9 +10,11 @@ export const getAllFilteredBuilds = async ({
   type?: BuildType;
   name?: string;
 }) => {
-  const response = await fetch(
-    `/api/builds/all-builds?name=${name}${category ? `&category=${category}` : ""}${type ? `&type=${type}` : ""}`,
-  );
+  const params = new URLSearchParams();
+  if (name) params.append("name", name);
+  if (category) params.append("category", category);
+  if (type) params.append("type", type);
+  const response = await fetch(`/api/builds/all-builds?${params}`);
 
   if (!response.ok) {
     throw new Error(`Failed to fetch all builds: ${response.status} ${response.statusText}`);
