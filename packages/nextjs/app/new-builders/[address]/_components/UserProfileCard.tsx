5146ac643a8a572302b088550eb0974306932292
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2025-06-12 07:43:11 +0800

    initial profile adjustments

---
+++ packages/nextjs/app/new-builders/[address]/_components/UserProfileCard.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+// import { UserLocation } from "./UserLocation";
+import { UserSocials } from "./UserSocials";
+import EditIcon from "~~/app/_assets/icons/EditIcon";
+import { UPDATE_USER_MODAL_ID, UpdateUserModal } from "~~/app/_components/UpdateUserModal";
+// import { PunkBlockie } from "~~/components/PunkBlockie";
+import { Address } from "~~/components/scaffold-eth/Address/Address";
+import { useAuthSession } from "~~/hooks/useAuthSession";
+import { BatchUserStatus } from "~~/services/database/config/types";
+import { Batch } from "~~/services/database/repositories/batches";
+import { UserByAddress } from "~~/services/database/repositories/users";
+
+export const UserProfileCard = ({ user, batch }: { user: NonNullable<UserByAddress>; batch: Batch }) => {
+  const { isAdmin } = useAuthSession();
+
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+        <div className="flex flex-col items-center gap-4 md:flex-row">
+          {/* <PunkBlockie address={user.userAddress} className="rounded-full" scale={1} /> */}
+          <div className="flex flex-col gap-3 text-neutral">
+            <Address address={user.userAddress} size="3xl" />
+            <UserSocials user={user} />
+          </div>
+        </div>
+        <div className="text-center md:text-right">
+          {batch && (
+            <div
+              className={`mb-2 inline-block rounded-sm px-2 py-0.5 font-semibold ${
+                user.batchStatus === BatchUserStatus.GRADUATE
+                  ? "bg-green-500/30"
+                  : "text-yellow-600 dark:text-yellow-400 bg-warning/30"
+              }`}
+            >
+              {batch.name.toString().toUpperCase()}
+            </div>
+          )}
+          {/* <UserLocation user={user} /> */}
+          {isAdmin && (
+            <>
+              <label htmlFor={UPDATE_USER_MODAL_ID} className="my-2 btn btn-xs btn-outline w-full">
+                <EditIcon className="w-3.5 h-3.5" />
+                Edit Profile
+              </label>
+            </>
+          )}
+          {/* <p className="m-0 text-sm text-neutral">
+            Joined {new Date(user.createdAt).toLocaleDateString("en-US", { month: "long", year: "numeric" })}
+          </p> */}
+        </div>
+      </div>
+      <UpdateUserModal user={user} />
+    </div>
+  );
+};
---
+++ packages/nextjs/app/new-builders/[address]/_components/UserSocials.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useRef } from "react";
+import { CopyValueToClipboard } from "../../../../components/CopyValueToClipboard";
+import { UpdateSocialsModal } from "./UpdateSocialsModal";
+import { useAccount } from "wagmi";
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import { UserByAddress } from "~~/services/database/repositories/users";
+import { getUserSocials, getUserSocialsList } from "~~/utils/socials";
+
+export const UserSocials = ({ user }: { user: NonNullable<UserByAddress> }) => {
+  const { address } = useAccount();
+  const modalRef = useRef<HTMLDialogElement>(null);
+  const isProfileOwner = address?.toLowerCase() === user.userAddress.toLowerCase();
+
+  const userSocials = getUserSocialsList(user);
+
+  if (!userSocials.length && !isProfileOwner) {
+    return null;
+  }
+
+  return (
+    <div className="flex items-center justify-center gap-2 md:justify-start md:ml-1">
+      {Boolean(userSocials.length > 0) && (
+        <div className="flex flex-wrap gap-3">
+          {userSocials
+            .filter(social => social.value)
+            .map(social => {
+              const link = social.getLink?.(social.value as string);
+              return (
+                <div key={social.key} className="flex items-center">
+                  {link ? (
+                    <a href={link} target="_blank" rel="noopener noreferrer" className="link">
+                      <social.icon className="w-6 h-6 opacity-70" />
+                    </a>
+                  ) : (
+                    <CopyValueToClipboard text={social.value as string} Icon={social.icon} />
+                  )}
+                </div>
+              );
+            })}
+        </div>
+      )}
+
+      {isProfileOwner && (
+        <button onClick={() => modalRef.current?.showModal()} className="btn btn-xs btn-ghost">
+          <PencilSquareIcon className="w-4 h-4" />
+        </button>
+      )}
+
+      <UpdateSocialsModal
+        ref={modalRef}
+        closeModal={() => modalRef.current?.close()}
+        existingSocials={getUserSocials(user)}
+      />
+    </div>
+  );
+};
