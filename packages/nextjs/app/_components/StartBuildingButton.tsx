2603f8716654bcbc7ff0e3d48aff14093bb3628a
Carlos Sánchez <oceanrdn@gmail.com>
2025-06-17 16:31:16 +0200

    Tweak hero

--- packages/nextjs/app/_components/StartBuildingButton.tsx
+++ packages/nextjs/app/_components/StartBuildingButton.tsx
@@ -4,7 +4,7 @@ import { useCallback } from "react";
 import Link from "next/link";
 import { usePlausible } from "next-plausible";
 
-export const StartBuildingButton = ({ firstChallengeId }: { firstChallengeId: string }) => {
+export const StartBuildingButton = () => {
   const plausible = usePlausible();
 
   // TODO: test this later
@@ -14,7 +14,7 @@ export const StartBuildingButton = ({ firstChallengeId }: { firstChallengeId: st
 
   return (
     <Link
-      href={`/challenge/${firstChallengeId}`}
+      href="/start"
       onClick={handleCtaClick}
       className="mt-4 px-6 py-3 text-lg font-medium text-white bg-primary rounded-full hover:bg-secondary-content dark:text-gray-800 transition-colors"
     >
