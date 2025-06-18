2be8428175728875383c862d07a4d597a7c0af40
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2025-06-18 20:20:30 +0800

    adding images and separations

--- packages/nextjs/app/start/page.tsx
+++ packages/nextjs/app/start/page.tsx
@@ -75,67 +75,138 @@ const StartLandingPage = () => {
       </div>
 
       <div className="w-full h-[18px] bg-[url('/assets/start/color-border.svg')] bg-repeat-x"></div>
-      <div className="py-12 bg-base-200 lg:py-24">
-        <div className="max-w-4xl mx-auto px-6 lg:pb-12">
-          <h2 className="text-center text-2xl font-semibold mb-4 md:text-4xl">Why build on Ethereum?</h2>
-          <p className="text-lg md:my-8 md:text-xl">
+      <div className="pt-12 bg-base-300 lg:pt-24">
+        <div className="max-w-4xl mx-auto px-6 text-lg lg:pb-12">
+          <h2 className="text-center text-2xl font-medium mb-4 md:text-4xl">
+            <Image
+              src="/assets/start/diamond.svg"
+              alt="Diamond"
+              aria-hidden="true"
+              className="relative inline-block w-10 h-10 -top-1 mr-1"
+              width={49}
+              height={49}
+            />{" "}
+            Why build on Ethereum?
+          </h2>
+          <p className="md:my-8 md:text-xl">
             Ethereum is a global network where you can deploy apps that <strong>run forever, can't be shut down</strong>
             , <strong>and give ownership to their creators.</strong>
           </p>
 
           <p className="mb-4">It works like this:</p>
-          <ul className="list-disc pl-6 mb-8">
+          <ul className="list-disc pl-6 mb-8 space-y-4">
             <li>
               🤖 You write logic in smart contracts and deploy them to the blockchain (decentralized, immutable,
               available to anyone forever!)
             </li>
-            <li>✏ You build a frontend to interact with your smart contracts</li>
+            <li>✏️ You build a frontend to interact with your smart contracts</li>
             <li>
               👥 Anyone in the world can use them or <strong>build on top</strong>: no gatekeepers, no approval needed.
             </li>
           </ul>
 
-          <p className="mb-8">
+          <p>
             It's not just about digital assets; Ethereum lets you build systems where{" "}
             <strong>money itself is programmable</strong>. From payments and rewards to auctions and governance, logic
             becomes value.
           </p>
         </div>
+        <div className="mt-12 relative h-[130px]">
+          <div className="absolute inset-0 bg-[url('/assets/header_platform.svg')] bg-repeat-x bg-[length:auto_130px] z-10" />
+          <div className="bg-base-100 absolute inset-0 top-auto w-full h-5" />
+        </div>
+      </div>
+      <div className="relative pt-12 bg-base-100 lg:pt-24">
+        <Image
+          src="/assets/start/clouds-left.svg"
+          alt="Clouds"
+          aria-hidden="true"
+          className="hidden absolute -left-24 scale-50 lg:left-0 lg:scale-100 xl:block"
+          width={332}
+          height={225}
+        />
+        <Image
+          src="/assets/start/clouds-right.svg"
+          alt="Clouds"
+          aria-hidden="true"
+          className="hidden absolute top-12 -right-32 scale-50 lg:block lg:scale-100 xl:-right-0"
+          width={445}
+          height={280}
+        />
+        <div className="relative z-10 max-w-4xl mx-auto px-6 text-lg lg:pb-12">
+          <h2 className="text-center text-2xl font-medium mb-4 md:text-4xl">
+            <Image
+              src="/assets/start/tools.svg"
+              alt="Tools"
+              aria-hidden="true"
+              className="relative inline-block w-10 h-10 -top-1 mr-1"
+              width={49}
+              height={49}
+            />{" "}
+            How does SpeedRunEthereum work?
+          </h2>
+          <p className="mt-8 mb-4">
+            You'll use{" "}
+            <a href="https://github.com/scaffold-eth/scaffold-eth-2" className="link">
+              Scaffold-ETH 2
+            </a>
+            , a powerful developer toolkit that gives you:
+          </p>
 
-        <h2 className="text-2xl font-bold mb-4">🛠 How does SpeedRunEthereum work?</h2>
-        <p className="mb-4">
-          You'll use{" "}
-          <a href="https://github.com/scaffold-eth/scaffold-eth-2" className="text-blue-500 hover:underline">
-            Scaffold-ETH 2
-          </a>
-          , a powerful developer toolkit that gives you:
-        </p>
+          <ul className="list-disc pl-6 mb-8 space-y-4">
+            <li>⚙️ A full Ethereum dev environment with local blockchain</li>
+            <li>🔌 A frontend connected to your contracts</li>
+            <li>🔍 Built-in Debug tools, Faucets, wallet connection, and powerful hooks and components.</li>
+          </ul>
 
-        <ul className="list-disc pl-6 mb-8">
-          <li>⚙️ A full Ethereum dev environment with local blockchain</li>
-          <li>🔌 A frontend connected to your contracts</li>
-          <li>🔍 Built-in Debug tools, Faucets, wallet connection, and powerful hooks and components.</li>
-        </ul>
+          <p>
+            You'll be able to <strong>tinker with smart contracts</strong>, deploy locally, test interactions, and build
+            usable decentralized apps from day one.
+          </p>
+          <p>
+            Along the way, you can submit completed challenges to your SpeedRunEthereum portfolio. Each challenge
+            becomes a public proof of your learning.
+          </p>
 
-        <p className="mb-8">
-          You'll be able to <strong>tinker with smart contracts</strong>, deploy locally, test interactions, and build
-          usable decentralized apps from day one. Along the way, you can submit completed challenges to your
-          SpeedRunEthereum portfolio. Each challenge becomes a public proof of your learning.
-        </p>
+          <div>
+            <p>
+              Don't worry if you don't understand everything up front. Each step is designed to make things click as you
+              go. Just keep going. It'll all start to make sense.
+            </p>
+          </div>
+        </div>
+        <div className="mt-12 relative h-[130px]">
+          <div className="absolute inset-0 bg-[url('/assets/start/separation-trees.svg')] bg-repeat-x bg-[length:auto_130px] z-10" />
+          <div className="bg-base-200 absolute inset-0 top-auto w-full h-24" />
+        </div>
+      </div>
 
-        <blockquote className="border-l-4 border-gray-300 pl-4 mb-8 italic">
-          Don't worry if you don't understand everything up front. Each step is designed to make things click as you go.
-          Just keep going. It'll all start to make sense.
-        </blockquote>
-
-        <h2 className="text-2xl font-bold mb-4">🚀 Ready?</h2>
-        <p className="mb-4">
-          Start with <strong>Challenge #0: Simple NFT Example</strong>
-        </p>
-        <p className="mb-4">You'll deploy your first smart contract and mint an NFT on a testnet.</p>
-        <p>
-          <StartChallengesButton />
-        </p>
+      <div className="bg-base-200 py-12 lg:py-24">
+        <div className="max-w-4xl mx-auto px-6 text-lg lg:pb-12">
+          <h2 className="text-center text-2xl font-medium mb-4 md:text-4xl">
+            <Image
+              src="/assets/start/spaceship.svg"
+              alt="Spaceship"
+              aria-hidden="true"
+              className="relative inline-block w-10 h-10 -top-1 mr-1"
+              width={47}
+              height={49}
+            />{" "}
+            Ready?
+          </h2>
+          <Image
+            src="/assets/challenges/simpleNFT.svg"
+            alt="NFT"
+            className="my-8 mx-auto max-w-md"
+            width={650}
+            height={400}
+          />
+          <p className="mb-2 text-center font-medium">Start with Challenge #0: Simple NFT Example</p>
+          <p className="mt-2 text-center">You'll deploy your first smart contract and mint an NFT on a testnet.</p>
+          <p className="mt-8 text-center">
+            <StartChallengesButton />
+          </p>
+        </div>
       </div>
     </div>
   );
