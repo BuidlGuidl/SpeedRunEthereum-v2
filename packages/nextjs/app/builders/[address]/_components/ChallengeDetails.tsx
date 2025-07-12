b218a174e8b6e311de60c9228b93a91ebb8cf5a4
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2025-07-12 11:09:36 +0800

    center challenge details button

--- packages/nextjs/app/builders/[address]/_components/ChallengeDetails.tsx
+++ packages/nextjs/app/builders/[address]/_components/ChallengeDetails.tsx
@@ -32,21 +32,27 @@ export function ChallengeDetails({
 
   return (
     <div className="text-base-content/50 hover:text-base-content transition">
-      <div className="mt-4 flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <div className="flex items-center justify-center w-6 h-6 bg-base-300 rounded-full font-semibold text-sm dark:bg-base-200">
-            {challenge.sortOrder}
+      <div className="mt-4 flex items-center justify-between gap-4">
+        <div>
+          <div className="flex items-center gap-2">
+            <div className="flex items-center justify-center w-6 h-6 bg-base-300 rounded-full font-semibold text-sm dark:bg-base-200">
+              {challenge.sortOrder}
+            </div>
+            <h2 className="m-0 font-medium">
+              {!isChallengeLocked && (
+                <Link href={`/challenge/${challenge.id}`} className="hover:underline">
+                  {challenge.challengeName}
+                </Link>
+              )}
+              {isChallengeLocked && challenge.challengeName}
+              {comingSoon && ` - Coming Soon`}
+            </h2>
           </div>
-          <h2 className="m-0 font-medium">
-            {!isChallengeLocked && (
-              <Link href={`/challenge/${challenge.id}`} className="hover:underline">
-                {challenge.challengeName}
-              </Link>
-            )}
-            {isChallengeLocked && challenge.challengeName}
-            {comingSoon && ` - Coming Soon`}
-          </h2>
+          <div className="pl-8">
+            <p className="mt-2 mb-0 text-sm">{shortedDescription}.</p>
+          </div>
         </div>
+
         {isChallengeLocked && isUserConnected && (
           <button disabled className="btn btn-sm rounded-md opacity-65 disabled:bg-accent disabled:text-accent-content">
             Locked
@@ -57,9 +63,6 @@ export function ChallengeDetails({
             Start
           </Link>
         )}
-      </div>
-      <div className="pl-8">
-        <p className="mt-2 mb-0 text-sm">{shortedDescription}.</p>
       </div>
     </div>
   );
