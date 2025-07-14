b10bf45e90a6d3eb36126ee333e1bbf7a19d3c7e
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2025-07-14 17:32:20 +0800

    Compact Builder Profile Challenges (#227)

--- packages/nextjs/app/builders/[address]/_components/ChallengeDetails.tsx
+++ packages/nextjs/app/builders/[address]/_components/ChallengeDetails.tsx
@@ -28,23 +28,31 @@ export function ChallengeDetails({
 
   const isUserConnected = connectedAddress === address;
 
+  const shortedDescription = challenge.description.split(".")[0];
+
   return (
     <div className="text-base-content/50 hover:text-base-content transition">
-      <div className="mt-6 flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <div className="flex items-center justify-center w-9 h-9 bg-base-300 rounded-full font-semibold lg:text-lg dark:bg-base-200">
-            {challenge.sortOrder}
+      <div className="mt-3 flex items-center justify-between gap-4">
+        <div>
+          <div className="flex items-center gap-2">
+            <div className="flex items-center justify-center w-6 h-6 bg-base-300 rounded-full font-semibold text-sm dark:bg-base-200">
+              {challenge.sortOrder}
+            </div>
+            <h2 className="m-0 font-medium">
+              {!isChallengeLocked && (
+                <Link href={`/challenge/${challenge.id}`} className="hover:underline">
+                  {challenge.challengeName}
+                </Link>
+              )}
+              {isChallengeLocked && challenge.challengeName}
+              {comingSoon && ` - Coming Soon`}
+            </h2>
           </div>
-          <h2 className="m-0 font-medium lg:text-xl">
-            {!isChallengeLocked && (
-              <Link href={`/challenge/${challenge.id}`} className="hover:underline">
-                {challenge.challengeName}
-              </Link>
-            )}
-            {isChallengeLocked && challenge.challengeName}
-            {comingSoon && ` - Coming Soon`}
-          </h2>
+          <div className="pl-8">
+            <p className="mt-2 mb-0 text-sm">{shortedDescription}.</p>
+          </div>
         </div>
+
         {isChallengeLocked && isUserConnected && (
           <button disabled className="btn btn-sm rounded-md opacity-65 disabled:bg-accent disabled:text-accent-content">
             Locked
@@ -55,9 +63,6 @@ export function ChallengeDetails({
             Start
           </Link>
         )}
-      </div>
-      <div className="pl-10">
-        <p>{challenge.description}</p>
       </div>
     </div>
   );
--- packages/nextjs/app/builders/[address]/_components/ChallengeDetailsStatus.tsx
+++ packages/nextjs/app/builders/[address]/_components/ChallengeDetailsStatus.tsx
@@ -17,30 +17,32 @@ export function ChallengeDetailsStatus({ challenge }: { challenge: MappedChallen
   if (isAccepted) statusAlertClass = "bg-[#E3FFF1] dark:bg-base-200/70";
   if (isRejected) statusAlertClass = "bg-error/20 dark:bg-error/50";
 
+  const shortedDescription = challenge.description.split(".")[0];
+
   return (
     <div>
-      <div className="mt-6 flex flex-wrap items-center justify-between">
+      <div className="mt-3 flex flex-wrap items-center justify-between">
         <div className="flex items-center gap-2">
-          <div className="flex items-center justify-center w-9 h-9 bg-base-300 rounded-full font-semibold lg:text-lg dark:bg-base-200">
+          <div className="flex items-center justify-center w-6 h-6 bg-base-300 rounded-full font-semibold text-sm dark:bg-base-200">
             {challenge.sortOrder}
           </div>
-          <h2 className="m-0 font-medium lg:text-xl">
+          <h2 className="m-0 font-medium">
             <Link href={`/challenge/${challenge.id}`} className="hover:underline">
               {challenge.challengeName}
             </Link>
           </h2>
         </div>
-        <div className={`badge py-4 px-3 lg:px-4 ${badgeClass}`}>{challenge.reviewAction}</div>
+        <div className={`badge badge-sm py-3 px-3 ${badgeClass}`}>{challenge.reviewAction}</div>
       </div>
-      <div className="pl-10">
-        <p>{challenge.description}</p>
-        <div className={`mt-6 flex items-center gap-2 px-4 py-3 rounded-lg ${statusAlertClass}`}>
+      <div className="pl-8">
+        <p className="mt-2 mb-0 text-sm">{shortedDescription}.</p>
+        <div className={`mt-3 flex items-center gap-2 px-2 py-1 rounded-md ${statusAlertClass}`}>
           <div>
             {isAccepted && "✅"}
             {isRejected && "❌"}
             {isSubmitted && "⏳"}
           </div>
-          <div className="flex items-center gap-4 text-sm">
+          <div className="flex items-center gap-4 text-xs">
             <DateWithTooltip timestamp={challenge.submittedAt || new Date()} />
             {challenge.contractUrl && (
               <a href={challenge.contractUrl} target="_blank" rel="noopener noreferrer" className="link">
