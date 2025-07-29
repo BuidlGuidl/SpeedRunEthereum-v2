8bb4663453faccc377e1873793e6310c83e20b1c
Carlos Sánchez <oceanrdn@gmail.com>
2025-07-29 16:54:30 +0200

    Tooltip on submit button

--- packages/nextjs/app/challenge/[challengeId]/_components/SubmitChallengeButton.tsx
+++ packages/nextjs/app/challenge/[challengeId]/_components/SubmitChallengeButton.tsx
@@ -8,17 +8,26 @@ import { useUser } from "~~/hooks/useUser";
 export const SubmitChallengeButton = ({ challengeId }: { challengeId: string }) => {
   const submitChallengeModalRef = useRef<HTMLDialogElement>(null);
   const { address: connectedAddress } = useAccount();
-
   const { data: user, isLoading: isLoadingUser } = useUser(connectedAddress);
+
+  const isDisabled = !user || isLoadingUser;
+
   return (
     <>
-      <button
-        className="btn btn-sm sm:btn-md btn-primary text-secondary px-3 sm:px-4 mt-2 fixed bottom-8 inset-x-0 mx-auto w-fit text-xs sm:text-sm"
-        disabled={!user || isLoadingUser}
-        onClick={() => submitChallengeModalRef && submitChallengeModalRef.current?.showModal()}
+      <div
+        className={`${isDisabled ? "tooltip " : ""}fixed bottom-8 inset-x-0 mx-auto w-fit`}
+        data-tip={
+          isDisabled ? "Please connect your wallet and register in SpeedRunEthereum to submit a challenge" : undefined
+        }
       >
-        Submit challenge
-      </button>
+        <button
+          className="btn btn-sm sm:btn-md btn-primary text-secondary px-3 sm:px-4 mt-2 text-xs sm:text-sm"
+          disabled={isDisabled}
+          onClick={() => submitChallengeModalRef?.current?.showModal()}
+        >
+          Submit challenge
+        </button>
+      </div>
       <SubmitChallengeModal
         challengeId={challengeId}
         ref={submitChallengeModalRef}
