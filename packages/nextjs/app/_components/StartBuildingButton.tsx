1e33999853aee8b7f3a2a45efe20868a50259ec0
Carlos Sánchez <oceanrdn@gmail.com>
2025-07-30 16:28:00 +0200

    remove new line

--- packages/nextjs/app/_components/StartBuildingButton.tsx
+++ packages/nextjs/app/_components/StartBuildingButton.tsx
@@ -15,8 +15,7 @@ export const StartBuildingButton = () => {
     <Link
       href="/start"
       onClick={handleCtaClick}
-      className="mt-4 px-6 py-3 text-lg font-medium text-content bg-accent border-[3px] border-primary rounded-full hover:bg-accent-content 
-      hover:border-base-300 hover:text-base-300 dark:bg-base-200 transition-colors"
+      className="mt-4 px-6 py-3 text-lg font-medium text-content bg-accent border-[3px] border-primary rounded-full hover:bg-accent-content hover:border-base-300 hover:text-base-300 dark:bg-base-200 transition-colors"
     >
       Start Building on Ethereum
     </Link>
