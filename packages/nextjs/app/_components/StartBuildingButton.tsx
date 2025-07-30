a49828898409db7a5bbea45f9034ccd621332c71
Andrea Albiac <137904915+andrealbiac@users.noreply.github.com>
2025-07-30 16:29:04 +0200

    Tweak Start button on homepage (#260)

--- packages/nextjs/app/_components/StartBuildingButton.tsx
+++ packages/nextjs/app/_components/StartBuildingButton.tsx
@@ -15,7 +15,7 @@ export const StartBuildingButton = () => {
     <Link
       href="/start"
       onClick={handleCtaClick}
-      className="mt-4 px-6 py-3 text-lg font-medium text-white bg-primary rounded-full hover:bg-secondary-content dark:text-gray-800 transition-colors"
+      className="mt-4 px-6 py-3 text-lg font-medium text-content bg-accent border-[3px] border-primary rounded-full hover:bg-accent-content hover:border-base-300 hover:text-base-300 dark:bg-base-200 transition-colors"
     >
       Start Building on Ethereum
     </Link>
