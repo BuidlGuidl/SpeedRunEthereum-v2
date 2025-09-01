33e95eaab2b640cafdd4997eb5240aa511e0963e
Carlos Sánchez <oceanrdn@gmail.com>
2025-09-01 12:44:55 +0200

    Interval

--- packages/nextjs/services/database/scripts/update-onchain-data.ts
+++ packages/nextjs/services/database/scripts/update-onchain-data.ts
@@ -1,7 +1,7 @@
 import { db } from "../config/postgresClient";
 import { users } from "../config/schema";
 import * as dotenv from "dotenv";
-import { asc, isNull } from "drizzle-orm";
+import { desc, isNull } from "drizzle-orm";
 import * as path from "path";
 import { UserUpdate } from "~~/services/database/repositories/users";
 import { updateUser } from "~~/services/database/repositories/users";
@@ -9,21 +9,32 @@ import { fetchOnchainData } from "~~/services/onchainData";
 
 dotenv.config({ path: path.resolve(__dirname, "../../../.env.development") });
 
-if (!process.env.POSTGRES_URL) {
-  console.error("Error: POSTGRES_URL environment variable is not set");
+if (!process.env.POSTGRES_URL || !process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || !process.env.ZERION_API_KEY) {
+  console.error(
+    "Error: POSTGRES_URL, NEXT_PUBLIC_ALCHEMY_API_KEY, or ZERION_API_KEY environment variables are not set",
+  );
   process.exit(1);
 }
 
-const BATCH_SIZE = 10;
+const BATCH_SIZE = 8;
+const OFFSET_START = 0;
+const INTERVAL_TIME = 60_000;
 
 const updateOnchainData = async () => {
   const usersWithoutSidequests = await db.query.users.findMany({
     where: isNull(users.sideQuestsSnapshot),
     limit: BATCH_SIZE,
-    orderBy: [asc(users.createdAt)],
+    offset: OFFSET_START,
+    orderBy: [desc(users.createdAt)],
   });
 
   console.log(`Processing ${usersWithoutSidequests.length} users without sidequests`);
+
+  // Exit if no users found
+  if (usersWithoutSidequests.length === 0) {
+    console.log("No users without sidequests found - stopping");
+    process.exit(0);
+  }
 
   let errorCount = 0;
   for (const user of usersWithoutSidequests) {
@@ -35,7 +46,11 @@ const updateOnchainData = async () => {
       const updateData: UserUpdate = {
         ens: ensData?.name || undefined,
         ensAvatar: ensData?.avatar || undefined,
-        sideQuestsSnapshot,
+        sideQuestsSnapshot: {
+          ...user.sideQuestsSnapshot,
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          ...sideQuestsSnapshot!,
+        },
       };
 
       await updateUser(user.userAddress, updateData);
@@ -51,7 +66,13 @@ const updateOnchainData = async () => {
     }
   }
 
-  console.log(`Total errors: ${errorCount}`);
+  console.log(`-> Total errors: ${errorCount}`);
 };
 
+setInterval(() => {
+  updateOnchainData();
+  console.log(`==============================================`);
+  console.log(`Waiting ${INTERVAL_TIME}ms before next update`);
+  console.log(`==============================================`);
+}, INTERVAL_TIME);
 updateOnchainData();
