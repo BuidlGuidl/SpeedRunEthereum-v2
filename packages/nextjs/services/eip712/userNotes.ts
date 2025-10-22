370b829ab315da762e305973bbced077ceef048d
Carlos Sánchez <oceanrdn@gmail.com>
2025-10-22 10:03:06 +0200

    eip712 notes

---
+++ packages/nextjs/services/eip712/userNotes.ts
@@ -0,0 +1,81 @@
+import { isValidEip712Signature } from "./common";
+
+export const EIP_712_TYPED_DATA__CREATE_USER_NOTE = {
+  domain: {
+    name: "SpeedRunEthereum",
+    version: "1",
+  },
+  types: {
+    Message: [
+      { name: "action", type: "string" },
+      { name: "userAddress", type: "string" },
+      { name: "comment", type: "string" },
+    ],
+  },
+  primaryType: "Message",
+  message: {
+    action: "Create User Note",
+  },
+} as const;
+
+export const EIP_712_TYPED_DATA__DELETE_USER_NOTE = {
+  domain: {
+    name: "SpeedRunEthereum",
+    version: "1",
+  },
+  types: {
+    Message: [
+      { name: "action", type: "string" },
+      { name: "noteId", type: "string" },
+    ],
+  },
+  primaryType: "Message",
+  message: {
+    action: "Delete User Note",
+  },
+} as const;
+
+export async function isValidEIP712CreateUserNoteSignature({
+  address,
+  signature,
+  userAddress,
+  comment,
+}: {
+  address: string;
+  signature: `0x${string}`;
+  userAddress: string;
+  comment: string;
+}) {
+  const typedData = {
+    ...EIP_712_TYPED_DATA__CREATE_USER_NOTE,
+    message: {
+      ...EIP_712_TYPED_DATA__CREATE_USER_NOTE.message,
+      userAddress,
+      comment,
+    },
+    signature,
+  };
+
+  return await isValidEip712Signature({ typedData, address });
+}
+
+export async function isValidEIP712DeleteUserNoteSignature({
+  address,
+  signature,
+  noteId,
+}: {
+  address: string;
+  signature: `0x${string}`;
+  noteId: number;
+}) {
+  const typedData = {
+    ...EIP_712_TYPED_DATA__DELETE_USER_NOTE,
+    message: {
+      ...EIP_712_TYPED_DATA__DELETE_USER_NOTE.message,
+      noteId: noteId.toString(),
+    },
+    signature,
+  };
+
+  return await isValidEip712Signature({ typedData, address });
+}
