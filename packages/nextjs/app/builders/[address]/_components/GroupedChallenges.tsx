fd68f2cfdcaf81ee460909cb8790f8c2b929d056
Carlos Sánchez <oceanrdn@gmail.com>
2026-01-21 09:29:15 +0100

    Remove comingSoon prop

--- packages/nextjs/app/builders/[address]/_components/GroupedChallenges.tsx
+++ packages/nextjs/app/builders/[address]/_components/GroupedChallenges.tsx
@@ -93,7 +93,7 @@ export function GroupedChallenges({
                 return <ChallengeDetailsStatus key={challenge.id} challenge={challenge} />;
               }
 
-              return <ChallengeDetails key={challenge.id} address={address} challenge={challenge} comingSoon={false} />;
+              return <ChallengeDetails key={challenge.id} address={address} challenge={challenge} />;
             })}
           </div>
         </div>
