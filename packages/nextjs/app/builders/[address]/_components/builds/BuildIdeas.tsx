5a979788608737bfdac334af2c8d944cf722b9de
Carlos Sánchez <oceanrdn@gmail.com>
2026-02-18 09:05:26 +0100

    better builds section

--- packages/nextjs/app/builders/[address]/_components/builds/BuildIdeas.tsx
+++ packages/nextjs/app/builders/[address]/_components/builds/BuildIdeas.tsx
@@ -1,4 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
+
+const BUILD_IDEAS = [
+  {
+    name: "Multisig Wallet",
+    description: "Secure assets by requiring multiple accounts to 'vote' on transactions.",
+    imageUrl: "/assets/challenges/multiSig.svg",
+  },
+  {
+    name: "SVG NFT",
+    description: "Tinker around with cutting edge smart contracts that render SVGs in Solidity.",
+    imageUrl: "/assets/challenges/dynamicSvgNFT.svg",
+  },
+];
 
 export function BuildIdeas() {
   return (
@@ -13,6 +27,23 @@ export function BuildIdeas() {
         </Link>
         .
       </p>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-3">
+        {BUILD_IDEAS.map(build => (
+          <Link
+            key={build.name}
+            href="/build-prompts"
+            className="bg-base-300 rounded-lg shadow-md overflow-hidden [&_img]:opacity-50 [&_img]:hover:opacity-100 text-base-content/50 hover:text-base-content hover:shadow-lg dark:bg-base-200"
+          >
+            <div className="pt-5 pb-4 w-full flex items-center justify-center bg-base-200">
+              <Image alt={build.name} src={build.imageUrl} width={652} height={401} className="w-3/4 mx-auto" />
+            </div>
+            <div className="flex flex-col flex-1 px-6 py-4">
+              <h2 className="text-xl font-bold mb-2 leading-tight">{build.name}</h2>
+              <p className="text-sm my-1">{build.description}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
